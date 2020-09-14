c736f95a7643f589ef50609f5b474d8faecf79ec
KOSTA <KOSTA@192.168.0.104>
2020-09-14 15:17:11 +0900

    두번째 수정 Test.java 추가 StartView.java 수정

--- src/kosta/mvc/view/StartView.java
+++ src/kosta/mvc/view/StartView.java
@@ -3,6 +3,7 @@ package kosta.mvc.view;
 public class StartView {
 	public static void main(String[] args) {
 		 MenuView.menu();
+		 System.out.println("^*^");
 	}
 	
 	
---
+++ src/kosta/mvc/view/Test.java
@@ -0,0 +1,8 @@
+package kosta.mvc.view;
+
+public class Test {
+	public void aa() {
+		System.out.println("¾È³ç");
+	}
+
+}
